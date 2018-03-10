c7f9344344da2cf3192dbabbd9b807e11ba40e2c
maritimer1975 <37162658+maritimer1975@users.noreply.github.com>
2018-03-10 16:03:18 -0400

    Test game physics

---
+++ Air Hockey Game/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	Vector3 pos;
+	[SerializeField] float speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis("Horizontal") * speed;
+		float z = Input.GetAxis("Vertical") * speed;
+
+		pos = new Vector3(x, 0, z);
+
+		transform.Translate(pos.x, 0, pos.z);
+	}
+}
